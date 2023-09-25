37adb8754c386a52fb4b00936501709f49bcd63a
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-09-25 11:42:57 +0200

    create projects data

---
+++ config/db.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'projects' => [
+        [
+            'title' => 'boolbnb-front-end',
+            'slug' => 'boolbnb-front-end',
+            'description' => '',
+            'url_github' => '',
+            'main_img' => '',
+            'visible' => true,
+            'user_id' => 1,
+        ],
+        [
+            'title' => 'boolbnb-back-end',
+            'slug' => 'boolbnb-back-end',
+            'description' => '',
+            'url_github' => '',
+            'main_img' => '',
+            'visible' => true,
+            'user_id' => 1,
+        ],
+        [
+            'title' => 'portfolio-vue-web',
+            'slug' => 'portfolio-vue-web',
+            'description' => '',
+            'url_github' => '',
+            'main_img' => '',
+            'visible' => true,
+            'user_id' => 1,
+        ],
+        [
+            'title' => 'portfolio-laravel-web',
+            'slug' => 'portfolio-laravel-web',
+            'description' => '',
+            'url_github' => '',
+            'main_img' => '',
+            'visible' => true,
+            'user_id' => 1,
+        ],
+        [
+            'title' => 'php-strong-password-generator',
+            'slug' => 'php-strong-password-generator',
+            'description' => '',
+            'url_github' => 'https://github.com/AlessandroPagliuca/php-strong-password-generator',
+            'main_img' => '',
+            'visible' => true,
+            'user_id' => 1,
+        ],
+    ]
+];
